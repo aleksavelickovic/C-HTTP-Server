9748619df37d3b0fd2d9a34350e245a997d53da4
aleksavelickovic <velickovic.sr44.2023@uns.ac.rs>
2025-11-10 00:58:17 +0100

    Add socket binding and listening setup in main.c

--- main.c
+++ main.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 int main(void) {
     printf("Hello, World!\n");
 
     int sock_status = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in addr = {0};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(8080);
+    addr.sin_addr.s_addr = INADDR_ANY;
 
+    bind(sock_status, (struct sockaddr *) &addr, sizeof(addr));
+
+    listen(sock_status, 5);
 
     return 0;
 }
