91b203fb27910bedffc3e0c1c455e8244a9c0fc3
aleksavelickovic <velickovic.sr44.2023@uns.ac.rs>
2025-11-10 02:05:07 +0100

    Implement basic HTTP server with socket setup and response handling in main.c

--- main.c
+++ main.c
@@ -1,21 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 #include <unistd.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    int server_socket;
+    struct sockaddr_in server_addr;
+    char buffer[1024];
+    const char *response =
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/plain\r\n"
+            "Content-Length: 13\r\n"
+            "\r\n"
+            "Hello, world!";
 
-    int sock_status = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in addr = {0};
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(8080);
-    addr.sin_addr.s_addr = INADDR_ANY;
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0) {
+        perror("socket failed");
+        exit(1);
+    }
 
-    bind(sock_status, (struct sockaddr *) &addr, sizeof(addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8081); // Port 8081
+    server_addr.sin_addr.s_addr = INADDR_ANY; // Bind to all interfaces
 
-    listen(sock_status, 5);
+    if (bind(server_socket, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+        perror("bind failed");
+        close(server_socket);
+        exit(1);
+    }
 
+    listen(server_socket, 5);
+    printf("Server running on http://localhost:8081\n");
+
+    while (1) {
+        const int client_socket = accept(server_socket, NULL, NULL);
+        if (client_socket < 0) {
+            perror("accept failed");
+            continue;
+        }
+
+        read(client_socket, buffer, sizeof(buffer) - 1);
+        write(client_socket, response, strlen(response));
+        close(client_socket);
+    }
+
+    close(server_socket);
     return 0;
 }
